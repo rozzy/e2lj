dd099e4029594b7a99a6343ed39a1db2b0a6505d
Nikita Nikitin <berozzy@gmail.com>
2012-12-22 23:12:02 +0200

    Initial commit

---
+++ e2lj/configure.php
@@ -0,0 +1,13 @@
+<?php
+	return array(
+		'login' => 'navalny',
+		'password' => 'qwertykremlin',
+
+		'entry-prefix' => '',
+		'entry-postfix' => '<br/><br/><a href="{url}" title="{title}">Оригинал</a>',
+
+		'import-tags' => true,
+		'permanent-tags' => '',
+		
+		'additional-settings' => array () /* look `props` in p2lj() https://github.com/rozzy/p2lj#p2lj  */
+	);
---
+++ e2lj/init.php
@@ -0,0 +1,6 @@
+<?php
+	require_once 'p2lj.php';
+	
+	$e2lj_configure = require_once 'configure.php';
+	
+	print_r($e2lj_configure);
