5716bbfc977a28e55141ea4ee5a3ce53048457b3
Nikita Nikitin <berozzy@gmail.com>
2012-12-23 13:50:01 +0200

    Form

--- e2lj/configure.php
+++ e2lj/configure.php
@@ -1,13 +1,12 @@
 <?php
 	return array(
 		'login' => 'navalny',
-		'password' => 'qwertykremlin',
 
 		'entry-prefix' => '',
 		'entry-postfix' => '<br/><br/><a href="{url}" title="{title}">Оригинал</a>',
 
 		'import-tags' => true,
 		'permanent-tags' => '',
-		
+
 		'additional-settings' => array () /* look `props` in p2lj() https://github.com/rozzy/p2lj#p2lj  */
 	);
--- e2lj/init.php
+++ e2lj/init.php
@@ -3,4 +3,17 @@
 	
 	$e2lj_configure = require_once 'configure.php';
 	
-	print_r($e2lj_configure);
+	if ($_POST) echo p2lj($e2lj_configure['login'], $_POST['password'], $_POST['title'], $_POST['message'], array('tagslist' => $_POST['tags'])) ? 'Пост запощщен!' : 'Не запощщен!';
+	?>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<script type="text/javascript" src="e2lj.min.js"></script>
+	<form action="" method="post">
+		<input type="password" name="password" id="pass"> <input type="submit" value="e2lj" onclick="e2lj('#pass', 'h1', '.article', 'tags')">
+		<h1>Привет, я заголовок</h1>
+		<div class="article">А я текст этой записи!</div>
+		<ul class="tags">
+			<li>тег</li>
+			<li>тег</li>
+			<li>тег</li>
+		</ul>
+	</form>
