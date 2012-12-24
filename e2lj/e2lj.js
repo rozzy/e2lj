90a3b772b37c03c2d604e1b68ca389145a10c5b7
Nikita Nikitin <berozzy@gmail.com>
2012-12-24 14:12:51 +0200

    Added tag-separator

--- e2lj/e2lj.js
+++ e2lj/e2lj.js
@@ -1,20 +1,29 @@
-function e2lj (ljPassword, titleSelector, articleSelector, tagsList) {
-	var targetUrl  = 'e2lj.php';
-	$.post(
-		targetUrl, 
-		{
-			password: $(ljPassword).val(),
-			title: $(titleSelector).text(),
-			message: $(articleSelector).html(),
-			tags: e2parseTags(tagsList)
-		},
-		function (response) {
-			console.log(response);
-		}, 
-		'json'
-	);
+function e2lj(ljPassword, titleSelector, articleSelector, tagsList) {
+    var targetUrl = 'e2lj.php';
+    $.post(
+    targetUrl, {
+        password: $(ljPassword).val(),
+        title: $(titleSelector).text(),
+        message: $(articleSelector).html(),
+        tags: e2parseTags(tagsList)
+    },
+
+    function (response) {
+        console.log(response);
+    },
+        'json');
 }
 
-function e2parseTags (selector) {
-	return $(selector).text();
+function e2parseTags(selector) {
+    var output = '';
+
+	if ($(selector).data('output') != 'line') { // Place data-output="line" to the element for parsing it as line
+		var child = $(selector).children().get(0).tagName;
+
+	   	$(child, selector).each(function () {
+	        output += $(this).text() + ',';
+	    });
+    } else output = $(selector).text();
+
+    return output.replace(/\,\s{1,}/gi, ',').replace(/\s{1,}\,/gi, ',').replace(/\,{2,}/, ',').replace(/\.|\,{1,}$/gi,'');
 }
--- e2lj/e2lj.php
+++ e2lj/e2lj.php
@@ -5,7 +5,11 @@
 	
 	if ($_POST) {
 		header('Content-type: application/json');
-		$post = p2lj($e2lj_configure['login'], $_POST['password'], $_POST['title'], $_POST['message'], array('tagslist' => $_POST['tags']));
+		echo json_encode($_POST);
+		exit;
+
+
+		$post = p2lj($e2lj_configure['login'], $_POST['password'], $_POST['title'], $_POST['message']);
 		echo is_array($post) ?
 			 json_encode(
 				array(
@@ -24,9 +28,14 @@
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
 	<script type="text/javascript" src="e2lj.js"></script>
 	
-	<input type="password" name="password" id="pass"> <input type="button" value="e2lj" onclick="e2lj('#pass', 'h1', '.article', 'tags')">
+	<input type="password" name="password" id="pass"> <input type="button" value="e2lj" onclick="e2lj('#pass', 'h1', '.article', '.tags')">
 	<h1>Привет, я заголовок</h1>
 	<div class="article">А я текст этой записи!</div>
+	<div class="tagline" data-output="line">
+		<a href="#">test</a>, 
+		<a href="#">test</a>, 
+		<a href="#">test</a>.
+	</div>
 	<ul class="tags">
 		<li>тег</li>
 		<li>тег</li>
